cb19e4eb884bdadce550de8e1c5a83f0a8e062c6
Boboye <boboye771@gmail.com>
2024-02-03 21:23:12 +0100

    changed font of small screen text

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NoButton from './components/NoButton/NoButton';
 import YesButton from './components/YesButton/YesButton';
 import Accepted from './components/Accepted/Accepted';
 import theBookOfLove from "./The-Book-of-Love.mp3"
+import SmallScreen from './components/SmallScreen/SmallScreen';
 
 function App() {
   const [isAccepted, setIsAccepted] = useState(false)
@@ -17,7 +18,7 @@ function App() {
     <div className="app">
       {
         (window.innerWidth > 768) ? <>
-          <h1 className='header'>WILL YOU BE MY VALENTINE</h1>
+          <h1 className='header'>WILL YOU BE MY VALENTINE? &#129402;</h1>
           {!isAccepted ? <div className='choice'>
             <YesButton setIsAccepted={setIsAccepted} playSound={playSound} stopSound={stop} />
             <NoButton />
@@ -26,7 +27,7 @@ function App() {
               <Accepted setIsAccepted={setIsAccepted} playSound={playSound} stopSound={stop} />
             </div>
           </>}
-        </> : <div>Small screen</div>
+        </> : <SmallScreen />
       }
     </div>
   );
