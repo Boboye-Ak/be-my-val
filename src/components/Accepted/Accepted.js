07f789567735d75e281f88ed13ae7ff74a3ae7ac
Boboye <boboye771@gmail.com>
2024-02-04 22:46:19 +0100

    added typewriter effect

--- src/components/Accepted/Accepted.js
+++ src/components/Accepted/Accepted.js
@@ -1,13 +1,16 @@
 import "./Accepted.css"
 import { Icon } from '@iconify/react';
 import { motion } from "framer-motion"
+import Typewriter from 'typewriter-effect';
 const Accepted = ({ setIsAccepted, playSound, stopSound }) => {
     return <motion.div className="accepted" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
         <div className="close-icon" onClick={() => {
             setIsAccepted(false)
             stopSound()
         }}><Icon icon="mi:close" /></div>
-        <div className="text">Good Choice. God saved you.</div>
+        <div className="text"> <Typewriter onInit={(typewriter) => {
+            typewriter.typeString("Good Choice. God saved you.").pauseFor(3000).deleteAll().typeString("JK. Thank you for saying yes. <3 :)").start()
+        }} /></div>
 
     </motion.div>
 }
