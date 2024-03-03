0cd71fe7cffc0d2e01020e5f7db1a03d390cac9e
Boboye <boboye771@gmail.com>
2024-03-03 11:41:03 +0100

    moved music file to cloud

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import './App.css';
 import NoButton from './components/NoButton/NoButton';
 import YesButton from './components/YesButton/YesButton';
 import Accepted from './components/Accepted/Accepted';
-import marriedLifeClip from "./married-life-clipped.mp3"
-
 import { motion } from "framer-motion"
 
 function App() {
+  const marriedLifeClip = "https://res.cloudinary.com/dc55ir792/video/upload/v1709462050/married-life-clipped_wtz4io.mp3"
   const [isAccepted, setIsAccepted] = useState(false)
   const [playSound, { stop }] = useSound(marriedLifeClip);
-
-
-
 
   return (
     <motion.div className="app" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
